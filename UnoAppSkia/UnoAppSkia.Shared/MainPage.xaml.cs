55af454454d6cd13cc1390141200561333b195b7
btord <btord@DEVELOP>
2022-07-05 16:39:08 -0500

    Add project files.

---
+++ UnoAppSkia/UnoAppSkia.Shared/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using SkiaSharp.Views.Windows;
+using SkiaSharp;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UnoAppSkia
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnPaintCanvas(object sender, SKPaintSurfaceEventArgs e)
+        {
+            using var p = new SKPaint { Color = SKColors.Blue };
+            e.Surface.Canvas.DrawText("hola", new SKPoint(100, 100), p);
+        }
+    }
+}
